2a0b21c758f2f31afe6dd37743f72b74f6bd4064
PatNoO <Noordh94@hotmail.se>
2025-10-21 15:08:49 +0200

    Added method getStringName in Class InputHandler

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class InputHandler {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getStringName(String text) {
+
+        while (true) {
+            String input = scanner.nextLine();
+            boolean isValid = true;
+
+            for (char c : input.toCharArray()) {
+                if (!Character.isLetter(c) &&
+                        c != '-' && c != '\'' && c != 'å' && c != 'ä' && c != 'ö' &&
+                        c != 'Å' && c != 'Ä' && c != 'Ö' ){
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid && !input.isEmpty()) {
+                return input;
+            } else {
+                System.out.println(text);
+            }
+        }
+    }
+
 }
