96eccd9e51f5718f7413e9359e831dca6f56a0ca
jonathan strid <pjstrid@outlook.com>
2025-10-23 16:42:41 +0100

    Changed some colors

--- src/Color.java
+++ src/Color.java
@@ -1,60 +1,62 @@
 public class Color {
 
-    public static final String RESET = "\u001B[0m";
+    public static final String RESET            = "\u001B[0m";
 
     ///  Text Color
 
-    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
-    public static final String GRAY = "\u001B[90m";
+    public static final String BLACK            = "\u001B[30m";
+    public static final String RED              = "\u001B[31m";
+    public static final String GREEN            = "\u001B[32m";
+    public static final String YELLOW           = "\u001B[33m";
+    public static final String BLUE             = "\u001B[34m";
+    public static final String PURPLE           = "\u001B[35m";
+    public static final String CYAN             = "\u001B[36m";
+    public static final String WHITE            = "\u001B[37m";
+    public static final String GRAY             = "\u001B[90m";
+
+    public static final String BRIGHT_RED       = "\u001B[91m";
+    public static final String BRIGHT_GREEN     = "\u001B[92m";
+    public static final String BRIGHT_YELLOW    = "\u001B[93m";
+    public static final String BRIGHT_BLUE      = "\u001B[94m";
+    public static final String BRIGHT_PURPLE    = "\u001B[95m";
+    public static final String BRIGHT_CYAN      = "\u001B[96m";
+    public static final String BRIGHT_WHITE     = "\u001B[97m";
+
+    public static final String ORANGE           = "\u001B[38;5;208m";
+    public static final String LIME_GREEN       = "\u001B[38;5;118m";
+    public static final String SKY_BLUE         = "\u001B[38;5;117m";
+    public static final String VIOLET           = "\u001B[38;5;177m";
+    public static final String CHARCOAL_GRAY    = "\u001B[38;5;236m";
+    public static final String LIGHT_GRAY       = "\u001B[38;5;250m";
+    public static final String HOT_PINK         = "\u001B[38;5;205m";
+    public static final String TURQUOISE        = "\u001B[38;5;80m";
 
     ///  Background color
 
-    public static final String BLACK_BG     = "\u001B[40m";
-    public static final String RED_BG       = "\u001B[41m";
-    public static final String GREEN_BG     = "\u001B[42m";
-    public static final String YELLOW_BG    = "\u001B[43m";
-    public static final String BLUE_BG      = "\u001B[44m";
-    public static final String PURPLE_BG    = "\u001B[45m";
-    public static final String CYAN_BG      = "\u001B[46m";
-    public static final String WHITE_BG     = "\u001B[47m";
-    public static final String GRAY_BG      = "\u001B[100m";
+    public static final String BLACK_BG         = "\u001B[40m";
+    public static final String RED_BG           = "\u001B[41m";
+    public static final String GREEN_BG         = "\u001B[42m";
+    public static final String YELLOW_BG        = "\u001B[43m";
+    public static final String BLUE_BG          = "\u001B[44m";
+    public static final String PURPLE_BG        = "\u001B[45m";
+    public static final String CYAN_BG          = "\u001B[46m";
+    public static final String WHITE_BG         = "\u001B[47m";
+    public static final String GRAY_BG          = "\u001B[100m";
 
-    public static final String BRIGHT_RED = "\u001B[91m";
-    public static final String BRIGHT_GREEN = "\u001B[92m";
-    public static final String BRIGHT_YELLOW = "\u001B[93m";
-    public static final String BRIGHT_BLUE  = "\u001B[94m";
-    public static final String BRIGHT_PURPLE = "\u001B[95m";
-    public static final String BRIGHT_CYAN  = "\u001B[96m";
-    public static final String BRIGHT_WHITE = "\u001B[97m";
+    ///  Font styles
 
-    public static final String ORANGE        = "\u001B[38;5;208m";
-    public static final String LIME_GREEN    = "\u001B[38;5;118m";
-    public static final String SKY_BLUE      = "\u001B[38;5;117m";
-    public static final String VIOLET        = "\u001B[38;5;177m";
-    public static final String CHARCOAL_GRAY = "\u001B[38;5;236m";
-    public static final String LIGHT_GRAY    = "\u001B[38;5;250m";
-    public static final String HOT_PINK      = "\u001B[38;5;205m";
-    public static final String TURQUOISE     = "\u001B[38;5;80m";
+    public static final String SANE             = "\u001B[0m";
 
-    public static final String SANE = "\u001B[0m";
+    public static final String BOLD             = "\u001B[1m";
+    public static final String THIN             = "\u001B[2m";
 
-    public static final String BOLD = "\u001B[1m";
-    public static final String THIN = "\u001B[2m";
+    public static final String ITALIC           = "\u001B[3m";
+    public static final String UNDERLINE        = "\u001B[4m";
+    public static final String BLINK            = "\u001B[5m";
+    public static final String RAPID_BLINK      = "\u001B[6m";
+    public static final String SWAP_COLOR       = "\u001B[7m";
+    public static final String INVISIBLE_TEXT   = "\u001B[8m";
 
-    public static final String ITALIC = "\u001B[3m";
-    public static final String UNDERLINE = "\u001B[4m";
-    public static final String BLINK = "\u001B[5m";
-    public static final String RAPID_BLINK = "\u001B[6m";
-    public static final String SWAP_COLOR = "\u001B[7m";
-    public static final String INVISIBLE_TEXT = "\u001B[8m";
-
-    public static final String BOX = "\u001B[51m";
+    public static final String BOX              = "\u001B[51m";
 }
 
